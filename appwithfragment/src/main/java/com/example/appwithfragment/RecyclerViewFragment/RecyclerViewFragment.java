ac8e0c88c351489e53dabaaf5eaf5d42c4b10fd6
Evgeniya Konobeeva <zknbeeva@mail.ru>
2016-08-15 00:28:36 +0300

    commit8

--- appwithfragment/src/main/java/com/example/appwithfragment/RecyclerViewFragment/RecyclerViewFragment.java
+++ appwithfragment/src/main/java/com/example/appwithfragment/RecyclerViewFragment/RecyclerViewFragment.java
@@ -22,6 +22,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Toast;
 
 import com.example.appwithfragment.RecyclerViewFragment.adapterClasses.RecyclerViewAdapter;
@@ -63,10 +64,15 @@ public class RecyclerViewFragment extends Fragment implements GettingResults {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        activity = this.getActivity();
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        activity = this.getActivity();
+        super.onCreate(savedInstanceState);
     }
 
     @Nullable
